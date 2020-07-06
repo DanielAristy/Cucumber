3356fdafacfb69cd7a840df1b84609e376eab932
DanielAristy <danielariaty@gmail.com>
2020-07-06 18:36:03 -0500

    Primer caso de prueba, aun sin probar nada

---
+++ src/test/java/cucumberglue/Test.java
@@ -0,0 +1,28 @@
+package cucumberglue;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Test {
+
+    @Given("^El usuario se encuentra en la pagina de Home imalittletester$")
+    public void el_usuario_se_encuentra_en_la_pagina_de_Home_imalittletester() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("^Hace click sobre el boton de The little tester comics$")
+    public void hace_click_sobre_el_boton_de_The_little_tester_comics() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("^Se debe redirigir a la pantalla Comics$")
+    public void se_debe_redirigir_a_la_pantalla_Comics() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+}
